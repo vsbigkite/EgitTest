cd2816c1fd7d5f6ba22899c7d4c4041fb705eab0
vida <vida@bigkiteconsulting.com>
2013-06-21 14:36:07 -0400

    Checking updates...

--- EgitTest/src/HelloEgit.java
+++ EgitTest/src/HelloEgit.java
@@ -7,6 +7,7 @@ public class HelloEgit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hello Egit!!");
+		System.out.print("Just wanted to check!");
 
 	}
 
